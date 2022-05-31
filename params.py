e7eea196b37bfee62cf277346c295b861a892579
SelimDeniz <33800163+SelimDeniz@users.noreply.github.com>
2022-05-31 17:20:20 +0300

    Add files via upload

--- params.py
+++ params.py
@@ -7,12 +7,13 @@ import os
 #############################
 # DataSet Path
 #############################s
-target_signals_dir = '../../DataSet/mancini_piano/piano'
+target_signals_dir = 'dataset/'
 #############################
 # Model Params
 #############################
 model_prefix = 'exp1' # name of the model to be saved
-n_iterations = 100000
+#n_iterations = 100000
+n_iterations = 10000
 use_batchnorm = False
 lr_g = 1e-4
 lr_d = 3e-4 # you can use with discriminator having a larger learning rate than generator instead of using n_critic updates ttur https://arxiv.org/abs/1706.08500
